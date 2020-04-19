31fa6d9ee258495319aae20b64f85fbee7a93d19
alammiman <alammiman@Companynet.org>
2020-04-19 10:26:03 +0100

    _

---
+++ game.js
@@ -0,0 +1,1 @@
+export const screentext = 'Welcome to the adventure!';
--- game.jsx
+++ game.jsx
@@ -1,3 +1,4 @@
 import React from 'react'
+import { screentext } from './game.js'
 
-export const Game = () => <div>Welcome to the adventure!</div>
+export const Game = () => <div>{screentext}</div>
