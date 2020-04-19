8aeecfbc02c5bb5949f888d49fd7ab0e823c0ada
alammiman <alammiman@Companynet.org>
2020-04-19 19:18:17 +0100

    _

--- game.test.js
+++ game.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent, waitForDomChange } from '@testing-library/react'
 import { ContentService } from './content.service.mock.js'
 import { Game } from './game.jsx'
 
@@ -8,5 +8,15 @@ test('when game starts message is displayed', () => {
     const {getByText} = render(<Game contentService={ContentService} />);
 
     expect(getByText('Welcome to the adventure!')).toBeInTheDocument();
+});
+
+test.skip('When requesting help the available commands are listed.', () =>{
+    const {getByText} = render(<Game contentService={ContentService} />);
+
+    fireEvent.keyPress('q');
+
+    return waitForDomChange(() => {
+        expect(getByText('Help is at hand')).toBeInTheDocument();
+    });
 });
 
