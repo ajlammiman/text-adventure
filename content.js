8d1e5bc923a698492e71d6c0091a00381c138680
Adam Lammiman <adam.lammiman@fdbhealth.com>
2020-07-17 07:42:36 +0100

    Revert "_"
    This reverts commit 29f708b553273a6d333af258e3f5f36e7aadc390.

--- content.js
+++
@@ -1,9 +0,0 @@
-export const content = () => {
-  const welcomeContent = () => {
-    return '';
-  };
-
-  return {
-    welcomeContent
-  };
-};
--- game.jsx
+++ game.jsx
@@ -1,4 +1,3 @@
 import React from 'react';
-import content from './content.js';
 
-export const Game = () => <>{content.welcomeContent()}</>;
+export const Game = () => <>Welcome to the game!</>;
