e0e7b21e54ce77eef765acf7e79a562e8609c700
Lammiman <alammiman@Companynet.org>
2020-08-11 11:16:56 +0100

    _

--- game.tsx
+++ game.tsx
@@ -9,26 +9,34 @@ export const Game = () => {
       <div>{displayContent}</div>
       <button onClick={() => setDisplayContent(content.help)}>help</button>
       <Looking content={content} setDisplayContent={setDisplayContent} />
-      <button onClick={() => setDisplayContent(content.forwards)}>Move Forward</button>
-      <button onClick={() => setDisplayContent(content.backwards)}>Move Backwards</button>
-      <button onClick={() => setDisplayContent(content.movingLeft)}>Move Left</button>
-      <button onClick={() => setDisplayContent(content.movingRight)}>Move Right</button>
+      <Moving content={content} setDisplayContent={setDisplayContent} />
     </>
   );
 };
 
-interface ActionComponent {
+interface Actions {
   content: GameContent;
   setDisplayContent: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Looking = ({ content, setDisplayContent }: ActionComponent) => {
+const Looking = ({ content, setDisplayContent }: Actions) => {
   return (
     <>
       <button onClick={() => setDisplayContent(content.left)}>Look Left</button>
       <button onClick={() => setDisplayContent(content.right)}>Look Right</button>
       <button onClick={() => setDisplayContent(content.ahead)}>Look Ahead</button>
       <button onClick={() => setDisplayContent(content.behind)}>Look Behind</button>
+    </>
+  );
+};
+
+const Moving = ({ content, setDisplayContent }: Actions) => {
+  return (
+    <>
+      <button onClick={() => setDisplayContent(content.forwards)}>Move Forward</button>
+      <button onClick={() => setDisplayContent(content.backwards)}>Move Backwards</button>
+      <button onClick={() => setDisplayContent(content.movingLeft)}>Move Left</button>
+      <button onClick={() => setDisplayContent(content.movingRight)}>Move Right</button>
     </>
   );
 };
