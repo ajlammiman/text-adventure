1544dcc8af617ce635eda95cca97e8abeac82b18
Lammiman <alammiman@Companynet.org>
2020-10-12 12:29:31 +0100

    _

--- looking.tsx
+++ looking.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
-type Direction = 'left' | 'right' | 'forward' | 'backward';
+enum Direction {
+  Left,
+  Right,
+  Forward,
+  Backwards
+}
 
 export const Looking = ({ setDisplayContent }: { setDisplayContent: React.Dispatch<React.SetStateAction<string>> }) => (
   <>
-    <button onClick={() => Look(setDisplayContent, 'left')}>Left</button>
-    <button onClick={() => Look(setDisplayContent, 'right')}>Right</button>
+    <button onClick={() => Look(setDisplayContent, Direction.Left)}>Left</button>
+    <button onClick={() => Look(setDisplayContent, Direction.Right)}>Right</button>
   </>
 );
 
 function Look(setDisplayContent: React.Dispatch<React.SetStateAction<string>>, direction: Direction) {
-  if (direction === 'left') setDisplayContent('I am looking to the left');
+  if (direction === Direction.Left) setDisplayContent('I am looking to the left');
 
-  if (direction === 'right') setDisplayContent('I am looking to the right');
+  if (direction === Direction.Right) setDisplayContent('I am looking to the right');
 }
