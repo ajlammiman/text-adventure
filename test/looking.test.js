655e2707ae540a1b3ff064d6ca4d84aa6ba0cfb5
Lammiman <alammiman@Companynet.org>
2020-10-12 14:04:59 +0100

    _

--- test/looking.test.js
+++ test/looking.test.js
@@ -3,6 +3,11 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Looking } from '../looking.tsx';
 
+const views = [
+  { direction: 'Left', description: 'A description of what I can see on my left' },
+  { direction: 'Right', description: 'A description of what I can see on my right' }
+];
+
 jest.mock('react', () => ({
   ...jest.requireActual('react'),
   useState: jest.fn()
@@ -13,16 +18,15 @@ describe('player can look', () => {
 
   beforeEach(() => {
     useStateMock.mockImplementation((init) => [init, setDisplayContent]);
+    render(<Looking setDisplayContent={setDisplayContent} views={views} />);
   });
 
   test('left', async () => {
-    render(<Looking setDisplayContent={setDisplayContent} />);
     fireEvent.click(await screen.findByText('Left'));
     expect(setDisplayContent).toHaveBeenCalledWith('A description of what I can see on my left');
   });
 
   test('right', async () => {
-    render(<Looking setDisplayContent={setDisplayContent} />);
     fireEvent.click(await screen.findByText('Right'));
     expect(setDisplayContent).toHaveBeenCalledWith('A description of what I can see on my right');
   });
