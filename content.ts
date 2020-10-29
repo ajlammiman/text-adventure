c93ce044eb59f2dfab1a67117294dd4b970205ad
Lammiman <alammiman@Companynet.org>
2020-10-29 11:56:42 +0000

    _

--- content.ts
+++ content.ts
@@ -4,12 +4,18 @@ interface Contents {
   start: string;
   help: string;
   views: Views;
+  location: Location;
 }
+
+type Location = {
+  description: string;
+};
 
 export const content = () => {
   return {
     start: 'Welcome to the game!',
     help: 'This is a help message',
-    views: []
+    views: [],
+    location: { description: 'Welcome to the game!' }
   } as Contents;
 };
